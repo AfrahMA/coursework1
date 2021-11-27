ded359418bdc6eeb0c13f9e3c298fec6ee4d8654
Afrah Mehveen <afrahmehveen@gmail.com>
2021-11-27 13:34:42 +0400

    Added subject.js file

---
+++ subjects.js
@@ -0,0 +1,84 @@
+let subjects = [
+    {
+      "id": 1001,
+      "lesson": "Math",
+      "location": "USA",
+      "price": 40.00,
+      "image": "math.png",
+      "spaces": 5
+    },
+    {
+        "id": 1002,
+        "lesson": "Biology",
+        "location": "UK",
+        "price": 30.00,
+        "image": "biology.jpg",
+        "spaces": 5
+    },
+    {
+        "id": 1003,
+        "lesson": "Physics",
+        "location": "UAE",
+        "price": 20.00,
+        "image": "physics.png",
+        "spaces": 5
+    },
+    {
+        "id": 1004,
+        "lesson": "Chemistry",
+        "location": "India",
+        "price": 40.00,
+        "image": "chemistry.jpg",
+        "spaces": 5
+    },  
+    {
+        "id": 1005,
+        "lesson": "English",
+        "location": "Scotland",
+        "price": 20.00,
+        "image": "english.jpg",
+        "spaces": 5
+    },
+    {
+        "id": 1006,
+        "lesson": "Geography",
+        "location": "Japan",
+        "price": 15.00,
+        "image": "geography.png",
+        "spaces": 5
+     },
+     {
+        "id": 1007,
+        "lesson": "Art",
+        "location": "Korea",
+        "price": 10.00,
+        "image": "art.jpg",
+        "spaces": 5
+     },
+     {
+        "id": 1008,
+        "lesson": "Information Technology",
+        "location": "Ireland",
+        "price": 10.00,
+        "image": "information technology.png",
+        "spaces": 5
+     },
+     {
+        "id": 1009,
+        "lesson": "Music",
+        "location": "Canada",
+        "price": 25.00,
+        "image": "music.png",
+        "spaces": 5
+        
+     },
+     {
+        "id": 1010,
+        "lesson": "Business Studies",
+        "location": "Australia",
+        "price": 23.00,
+        "image": "business studies.jpg",
+        "spaces": 5
+    },
+
+  ]
